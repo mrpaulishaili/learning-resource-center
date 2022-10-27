4080f6112acc8af41ba3f0caa8d8f97183456ecd
Mr. Paul Ishaili C <20476203+mrpaulishaili@users.noreply.github.com>
2022-10-27 11:29:33 +0100

    ..

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -1,12 +1,7 @@
-const env = () => {
-  if (window.location.hostname === 'localhost' || '127.0.0.1') {
-    return 'development';
-  } else return 'production';
-};
+const env = window.location.hostname.includes('localhost');
 
-const API_URL =
-  env === 'development'
-    ? 'http://localhost:5000'
-    : 'https://learning-resource-center-api.onrender.com/';
+const API_URL = env
+  ? 'http://localhost:5000'
+  : 'https://learning-resource-center-api.onrender.com';
 
 export { API_URL };
--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 import axios from 'axios';
-import { API_URL } from '../config/config';
 
 const Books = () => {
   const [books, setBooks] = useState([]);
@@ -10,7 +9,7 @@ const Books = () => {
   useEffect(() => {
     const fetchAllBooks = async () => {
       try {
-        const res = await axios.get(`${API_URL}/books`);
+        const res = await axios.get(`http://localhost:5000/books`);
 
         setBooks(res.data);
       } catch (error) {
